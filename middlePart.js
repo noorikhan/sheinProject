fb1bffa0ea601e4e6d7216443eb73476f3c9e012
sumit7195 <93507024+sumit7195@users.noreply.github.com>
2021-11-01 17:25:23 +0530

    Create middlePart.js
    upto shopby category

---
+++ middlePart.js
@@ -0,0 +1,35 @@
+// upper-black friday
+
+   var upperBlack = {
+           img1: "https://img.ltwebstatic.com/images3_acp/2021/08/23/1629714618589673af8828d6c6293f5524d60cf92b.gif",
+           img2: "https://img.ltwebstatic.com/images3_acp/2021/10/30/1635588008792771c353f0c00311e8c2c292b9a268.webp",
+           img3 : "https://img.ltwebstatic.com/images3_acp/2021/08/23/162971468501a39d853f9c1797089196a5514ba979.webp",
+     }
+
+
+   var blackImg = {
+
+               img1 : "https://img.ltwebstatic.com/images3_ach/2021/11/01/163573213408d427f781f85411867c124cd1148009.gif",
+   }
+
+
+ // shop-by-category 
+ 
+   var category = [
+   
+           {title:"Home Decor" , img:"https://cdn-icons.flaticon.com/png/128/942/premium/942165.png?token=exp=1635691733~hmac=29513a9347b3408d55f36a50da6cdaee", backgroundColor:"#F9F4F6"},
+           {title:"Bedding", img:"https://cdn-icons.flaticon.com/png/128/2284/premium/2284001.png?token=exp=1635692073~hmac=c67daeb11893e287c5c31dc499ffbe33",backgroundColor:"#F9F4F6" },
+           {title:"Kitchen", img:"https://cdn-icons-png.flaticon.com/128/864/864615.png",backgroundColor:"#F9F4F6"},
+           {title:"Lighting",img:"https://cdn-icons-png.flaticon.com/128/3997/3997723.png",backgroundColor:"#F9F4F6"},
+           {tile:"Events & Party", img:"https://cdn-icons-png.flaticon.com/128/3918/3918214.png",backgroundColor:"#F9F4F6"},
+           {title:"Home Entertainment",img:"https://cdn-icons.flaticon.com/png/128/738/premium/738822.png?token=exp=1635692421~hmac=e49ff98b642bddfb101b09a93be138bb",backgroundColor:"#F9F4F6"},
+           {titel:"Storage",img:"https://cdn-icons.flaticon.com/png/128/1602/premium/1602321.png?token=exp=1635692478~hmac=4b2b9a4dbe0db16dcac7871f88547d01",backgroundColor:"#F9F4F6"},
+           {title:"Bath",img:"https://cdn-icons-png.flaticon.com/128/637/637229.png",backgroundColor:"#F9F4F6"},  
+           {title:"Home Appliances", img:"https://cdn-icons-png.flaticon.com/128/1554/1554170.png",backgroundColor:"#F9F4F6"},
+           {title:"Essentials",img:"https://cdn-icons-png.flaticon.com/128/2917/2917024.png",backgroundColor:"#F9F4F6"},
+           {title:"Stationery",img:"https://cdn-icons-png.flaticon.com/128/2653/2653535.png",backgroundColor:"#F9F4F6"},
+           {title:"Pet Accessories", img:"https://cdn-icons.flaticon.com/png/128/2195/premium/2195893.png?token=exp=1635692823~hmac=1e707f38ab178fa00d67b9f241606065",backgroundColor:"#F9F4F6"},
+           {title:"Pet Toys",img:"https://cdn-icons.flaticon.com/png/128/4889/premium/4889164.png?token=exp=1635692911~hmac=133c903bb2784337d115e237562a527f",backgroundColor:"#F9F4F6"},
+           {title:"Pet Clothing", img:"https://cdn-icons.flaticon.com/png/128/1811/premium/1811895.png?token=exp=1635692993~hmac=30dd01bd7ba264be9db9853ecb1b0447",backgroundColor:"#F9F4F6"},
+           {title:"Pet OutDoor Gear", img:"https://t4.ftcdn.net/jpg/02/96/81/01/240_F_296810121_YL3xxYAiERbMHJHxdzxVmytcAKlZozuC.jpg",backgroundColor:"#F9F4F6"}
+   ]
