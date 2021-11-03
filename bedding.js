fa6641b1d36e9d0cc4d827e6df1c8190b6124113
sumit7195 <93507024+sumit7195@users.noreply.github.com>
2021-11-03 19:38:37 +0530

    this is upto bedding category

--- bedding.js
+++ bedding.js
@@ -1,9 +1,8 @@
+<<<<<<< HEAD
 
 
 
-// buttons
-
-
+//this is navbar buttons
 var buttons = [{title:"Bedding",backgroundColor:"white"},
               {title:"Kitchen & Dining", backgroundColor:"white"},
               {title:"Home Decor", backgroundColor:"white"},
@@ -17,7 +16,7 @@ var buttons = [{title:"Bedding",backgroundColor:"white"},
                {title:"Home Improvement",backgroundColor:"white"},
                {title:"Holiday",backgroundColor:"white"}
 ]
-
+//this is bedding image
 var bedding = [
     {title:"1 pair Christmas print Pillowcase", price:"US$10.00", img:["bedding image/img2.webp","bedding image/img2.1.webp","bedding image/img2.2.webp","bedding image/img2.4.webp" ]},
     {title:"1pc plain Plush Blanket",price:"US$54.00", img:["bedding image/img1.webp"]},
@@ -25,3 +24,33 @@ var bedding = [
     {title:"Butterfly Print Duvet Coever Set Without",price:"US$37.00",img:["bedding image/img5.webp","bedding image/img5.1.webp","bedding image/img5.2.webp"]},
     {title:"1pair Fleece Pillowcase Without Filler", price:"US$15.00",img:["bedding image/img3.webp","bedding image/img3.1.webp"]}
 ]
+
+
+
+// this is bedskirt category which is in bedding image
+var bedSkirt = [
+
+ {category:"bedSkirt", title:"Pom Pom Bed Skirt", price:"US$19.00", imgUrl:["img1.webp","img1.1.webp"]},
+ {category:"bedSkirt", title:"Plain Ruffle Bed Skirt",price:"US$24.00",imgUrl:["img2.webp"]},
+ {category:"bedSkirt", title:"Flower Print Bed Skirt",price:"US$19.00",imgUrl:["img3.webp"]},
+ {category:"bedSkirt", title:"Houndstooth pattern Bed Skirt",price:"US$35.00",imgUrl:["img4.webp"]},
+ {category:"bedSkirt", title:"Plain Print Bed Skirt",price:"US$35.00",imgUrl:["img5.webp"]}
+ ]
+=======
+// buttons
+
+var buttons = [
+  { title: "Bedding", backgroundColor: "white" },
+  { title: "Kitchen & Dining", backgroundColor: "white" },
+  { title: "Home Decor", backgroundColor: "white" },
+  { title: "Home Essentials", backgroundColor: "white" },
+  { title: "Stroage & Organization", backgroundColor: "white" },
+  { title: "lighting and lamp", backgroundColor: "white" },
+  { title: "Bathroom", backgroundColor: white },
+  { title: "Event party Supplies", backgroundColor: "white" },
+  { title: "Pet Supplies", backgroundColor: "white" },
+  { title: "SHIEN BASIC LIVING", backgroundColor: "white" },
+  { title: "Home Improvement", backgroundColor: "white" },
+  { title: "Holiday", backgroundColor: "white" },
+];
+>>>>>>> 7564dee2692d19c81ca1e98bfd36598e6c568e58
