9c5825f4231ef320a6b7f5b4a6c3b6b917d70218
Noori Fathima <noorifathima99@gmail.com>
2021-11-12 19:14:11 +0530

    category updated

--- cart.js
+++ cart.js
@@ -7,132 +7,128 @@ body.append(mainDiv);
 showProducts();
 
 if (cartData.length == 0) {
-	var msg = document.createElement("h1");
-	
-	msg.textContent = "Your cart is empty";
-	var emptyDiv = document.createElement("div");
-	emptyDiv.setAttribute("id","emptyDiv");
-    var emptyImg = document.createElement("img");
-	emptyImg.setAttribute("id","emptyImg");
-	emptyImg.setAttribute("src","images/emptyCart.png")
-	emptyDiv.append(emptyImg,msg)
-	body.append(emptyDiv);
+  var msg = document.createElement("h1");
+
+  msg.textContent = "Your cart is empty";
+  var emptyDiv = document.createElement("div");
+  emptyDiv.setAttribute("id", "emptyDiv");
+  var emptyImg = document.createElement("img");
+  emptyImg.setAttribute("id", "emptyImg");
+  emptyImg.setAttribute("src", "images/emptyCart.png");
+  emptyDiv.append(emptyImg, msg);
+  body.append(emptyDiv);
 }
 
 function showProducts() {
-	cartData.map(function (item) {
-		var box = document.createElement("div");
-             
-		var imgDiv = document.createElement("div");
+  cartData.map(function (item) {
+    var box = document.createElement("div");
 
-		imgDiv.setAttribute("class", "imageDive");
+    var imgDiv = document.createElement("div");
 
-		var image = document.createElement("img");
-		image.setAttribute("src", item.imgUrl[0]);
+    imgDiv.setAttribute("class", "imageDive");
 
-		image.addEventListener("click", function () {
-			productDetails(item);
-		}); 
+    var image = document.createElement("img");
+    image.setAttribute("src", item.imgUrl);
 
-		imgDiv.append(image);
+    image.addEventListener("click", function () {
+      productDetails(item);
+    });
 
-		var h = document.createElement("div");
-	    
-		h.textContent = item.title;
+    imgDiv.append(image);
 
-		var div = document.createElement("div");
-		div.setAttribute("id", "heartIcon");
+    var h = document.createElement("div");
 
-		var p = document.createElement("h4");
-		p.setAttribute("id","price")
-		p.textContent = `US $${item.price}`;
+    h.textContent = item.title;
 
-		box.append(imgDiv, h, p, div);
+    var div = document.createElement("div");
+    div.setAttribute("id", "heartIcon");
 
-		mainDiv.append(box);
-	});
+    var p = document.createElement("h4");
+    p.setAttribute("id", "price");
+    p.textContent = `US $${item.price}`;
+
+    box.append(imgDiv, h, p, div);
+
+    mainDiv.append(box);
+  });
 }
 
 if (cartData.length > 0) {
+  var orderSummary = document.createElement("div");
+  orderSummary.setAttribute("id", "orderSummary");
+  var totalPrice = document.createElement("h1");
+  var sumPrice = 0;
+  for (var i = 0; i < cartData.length; i++) {
+    sumPrice += +cartData[i].price;
+  }
+  totalPrice.textContent = `Total Price is: - US $${Math.floor(sumPrice)}`;
+  totalPrice.setAttribute("id", "totalPrice");
+  var promoCode = document.createElement("input");
+  promoCode.setAttribute("id", "promoCode");
+  promoCode.type = "text";
 
-	var orderSummary = document.createElement("div");
-	orderSummary.setAttribute("id","orderSummary");
-	var totalPrice = document.createElement("h1");
-	var sumPrice = 0;
-	for (var i = 0; i < cartData.length; i++) {
-		sumPrice += +cartData[i].price;
-	}
-	totalPrice.textContent = `Total Price is: - US $${Math.floor(sumPrice)}`;
-    totalPrice.setAttribute("id","totalPrice");
-	var promoCode = document.createElement("input");
-	promoCode.setAttribute("id","promoCode");
-	promoCode.type = "text";
+  var applyCode = document.createElement("button");
+  applyCode.textContent = "Apply Code";
 
-	var applyCode = document.createElement("button");
-	applyCode.textContent = "Apply Code";
-   
-	applyCode.addEventListener("click", function () {
-		var code = promoCode.value;
-		
+  applyCode.addEventListener("click", function () {
+    var code = promoCode.value;
 
-		// 10% off
-		if (sumPrice > 29 && sumPrice <= 69) {
-			if (code == "") {
-				alert("promo code field is empty");
-			} else if (code != "HOT1111") {
-				alert("wrong promo code");
-			} else {
-				sumPrice = sumPrice - sumPrice * 0.1;
-				totalPrice.textContent = `Total Price - US $${sumPrice}`;
-				promoCode.value = "";
-			}
-		}
-		// 15% off
-		else if (sumPrice > 69 && sumPrice <= 169) {
-			if (code == "") {
-				alert("promo code field is empty");
-			} else if (code != "HOT1111") {
-				alert("wrong promo code");
-			} else {
-				sumPrice = sumPrice - sumPrice * 0.15;
-				totalPrice.textContent = `Total Price - US $${sumPrice}`;
-				promoCode.value = "";
-			}
-		}
-		// 20% off
-		else {
-			if (code == "") {
-				alert("promo code field is empty");
-			} else if (code != "HOT1111") {
-				alert("wrong promo code");
-			} else {
-				sumPrice = sumPrice - sumPrice * 0.2;
-				totalPrice.textContent = `Total Price - US $${sumPrice}`;
-			
-				promoCode.value = "";
-			}
-		}
-	});
+    // 10% off
+    if (sumPrice > 29 && sumPrice <= 69) {
+      if (code == "") {
+        alert("promo code field is empty");
+      } else if (code != "HOT1111") {
+        alert("wrong promo code");
+      } else {
+        sumPrice = sumPrice - sumPrice * 0.1;
+        totalPrice.textContent = `Total Price - US $${sumPrice}`;
+        promoCode.value = "";
+      }
+    }
+    // 15% off
+    else if (sumPrice > 69 && sumPrice <= 169) {
+      if (code == "") {
+        alert("promo code field is empty");
+      } else if (code != "HOT1111") {
+        alert("wrong promo code");
+      } else {
+        sumPrice = sumPrice - sumPrice * 0.15;
+        totalPrice.textContent = `Total Price - US $${sumPrice}`;
+        promoCode.value = "";
+      }
+    }
+    // 20% off
+    else {
+      if (code == "") {
+        alert("promo code field is empty");
+      } else if (code != "HOT1111") {
+        alert("wrong promo code");
+      } else {
+        sumPrice = sumPrice - sumPrice * 0.2;
+        totalPrice.textContent = `Total Price - US $${sumPrice}`;
 
-	var checkOut = document.createElement("button");
-	checkOut.setAttribute("id","checkOut");
-	checkOut.textContent = "Checkout";
-     
-	checkOut.addEventListener("click", function () {
-		window.location.href = "checkout.html";
-	});
-	var order = document.createElement("h3");
-	order.setAttribute("id","order");
-	order.textContent = "Order Summay";
-    applyCode.setAttribute("id","applyCode");
-    orderSummary.append(order,totalPrice, promoCode, applyCode, checkOut)
-	body.append(orderSummary);
+        promoCode.value = "";
+      }
+    }
+  });
 
+  var checkOut = document.createElement("button");
+  checkOut.setAttribute("id", "checkOut");
+  checkOut.textContent = "Checkout";
+
+  checkOut.addEventListener("click", function () {
+    window.location.href = "checkout.html";
+  });
+  var order = document.createElement("h3");
+  order.setAttribute("id", "order");
+  order.textContent = "Order Summay";
+  applyCode.setAttribute("id", "applyCode");
+  orderSummary.append(order, totalPrice, promoCode, applyCode, checkOut);
+  body.append(orderSummary);
 }
 
-
 var weAccept = document.createElement("div");
-weAccept.setAttribute("id","weAccept");
+weAccept.setAttribute("id", "weAccept");
 var div1 = document.createElement("div");
- div1.setAttribute("src","")    
+div1.setAttribute("src", "");
 body.append(weAccept);
--- fav.js
+++ fav.js
@@ -7,47 +7,47 @@ body.append(mainDiv);
 showProducts();
 
 if (favProductsData.length == 0) {
-	var msg = document.createElement("h1");
-	msg.setAttribute("id","msg");
-	// var favImg = document.createElement("img");
-	// favImg.setAttribute("id","favImg");
-	// favImg.setAttribute("src","images/favIcon.png")
-	msg.textContent = "Nothing In Wishlist";
-	var favDiv = document.createElement("div");
-	favDiv.setAttribute("id","favDiv");
-	favDiv.append(msg);
-	body.append(favDiv);
+  var msg = document.createElement("h1");
+  msg.setAttribute("id", "msg");
+  // var favImg = document.createElement("img");
+  // favImg.setAttribute("id","favImg");
+  // favImg.setAttribute("src","images/favIcon.png")
+  msg.textContent = "Nothing In Wishlist";
+  var favDiv = document.createElement("div");
+  favDiv.setAttribute("id", "favDiv");
+  favDiv.append(msg);
+  body.append(favDiv);
 }
 
 function showProducts() {
-	favProductsData.map(function (item) {
-		var box = document.createElement("div");
+  favProductsData.map(function (item) {
+    var box = document.createElement("div");
 
-		var imgDiv = document.createElement("div");
-		imgDiv.setAttribute("class", "imageDive");
+    var imgDiv = document.createElement("div");
+    imgDiv.setAttribute("class", "imageDive");
 
-		var image = document.createElement("img");
-		image.setAttribute("src", item.imgUrl[0]);
+    var image = document.createElement("img");
+    image.setAttribute("src", item.imgUrl);
 
-		image.addEventListener("click", function () {
-			productDetails(item);
-		});
+    image.addEventListener("click", function () {
+      productDetails(item);
+    });
 
-		imgDiv.append(image);
+    imgDiv.append(image);
 
-		var h = document.createElement("div");
-		h.textContent = item.title;
-		h.setAttribute("id","title")
+    var h = document.createElement("div");
+    h.textContent = item.title;
+    h.setAttribute("id", "title");
 
-		var div = document.createElement("div");
-		div.setAttribute("id", "heartIcon");
+    var div = document.createElement("div");
+    div.setAttribute("id", "heartIcon");
 
-		var p = document.createElement("h4");
-		p.setAttribute("id","price")
-		p.textContent = `US $${item.price}`;
+    var p = document.createElement("h4");
+    p.setAttribute("id", "price");
+    p.textContent = `US $${item.price}`;
 
-		box.append(imgDiv, h, p, div);
+    box.append(imgDiv, h, p, div);
 
-		mainDiv.append(box);
-	});
+    mainDiv.append(box);
+  });
 }
