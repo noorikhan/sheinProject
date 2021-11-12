bf7e8ec1f50606c70c012c93f064e0ef5052621e
Noori Fathima <noorifathima99@gmail.com>
2021-11-12 20:56:30 +0530

    productDetailsPage separated as html,css,js

---
+++ productDetails.js
@@ -0,0 +1,35 @@
+var productObj = JSON.parse(localStorage.getItem("seleProd"));
+displayImgs();
+displayDetails();
+
+function displayImgs() {
+  var bImg = document.createElement("img");
+  bImg.setAttribute("src", productObj.imgUrl);
+  document.getElementById("img1").append(bImg);
+}
+
+function displayDetails() {
+  document.querySelector("h4").textContent = productObj.title;
+  document.querySelector("h1").textContent = `US$${productObj.price}`;
+  document.getElementById("size").innerHTML = `<h3>${productObj.size[0]}</h3>`;
+}
+
+var cartData = JSON.parse(localStorage.getItem("cartData")) || [];
+
+document.querySelector("button").addEventListener("click", function () {
+  var exist = false;
+  for (var j = 0; j < cartData.length; j++) {
+    if (productObj.title == cartData[j].title) {
+      exist = true;
+      break;
+    }
+  }
+  if (exist) {
+    alert("product is already added to cart");
+  } else {
+    alert("Item is added successfully");
+    cartData.push(productObj);
+
+    localStorage.setItem("cartData", JSON.stringify(cartData));
+  }
+});
